14990f1d3b7592d084c4434b14e22c9429b81488
Tamara <tlumumba@gmail.com>
2023-08-24 02:26:06 -0700

    Mitigating errors in main.c file

--- main.c
+++ main.c
@@ -1,3 +1,9 @@
+/*
+ * File: main.c
+ * Auth: Tamara Lumumba
+ */
+
+
 #include "monty.h"
 
 /**
@@ -17,31 +23,13 @@ int main(int argc, char *argv[])
 	char *file_name1 = argv[1];
 	FILE *file1 = fopen(file_name1, "r");
 
-	if (file == NULL)
+	if (file1 == NULL)
 	{
 		fprintf(stderr, "Error opening file: %s\n", filename);
 		exit(EXIT_FAILURE);
 	}
-	char *linebuffer = NULL;
-	size_t bufferSize = 0;
-	ssize_t bytesRead;
 
-	int linecounter = 0;
-	stack montystack;
+	int exit_status = execute_Monty(file1);
 
-	initStack(&montystack); /* initializing the stack*/
-
-	while ((byteRead = getline(&lineBuffer, &bufferSize, file1)) != -1)
-	{
-		lineCounter++;
-		if (byteRead > 0 && linebuffer[bytesRead - 1] == '\n')
-		{
-			lineBuffer[bytesRead - 1] = '\0';
-		}
-
-		executeMontyInstruction(lineBuffer, &montyStack);
-	}
-
-	fclose(inputFile);
-	free(lineBuffer);
-	freeStack(&montystack);
+	fclose(file1);
+	return (exit_status);
